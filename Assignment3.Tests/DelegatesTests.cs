825b005ea276e59c9aa6365a3cd9958e79727763
DarkLordOfDeadstiny <patr7500@gmail.com>
2021-09-27 16:06:18 +0200

    add delegates

--- Assignment3.Tests/DelegatesTests.cs
+++ Assignment3.Tests/DelegatesTests.cs
@@ -1,14 +1,50 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
+using System.Linq;
+using System.IO;
 
 namespace BDSA2021.Assignment03.Tests
 {
     public class DelegatesTests
     {
         [Fact]
-        public void Test1()
+        public void reversed_numbers_in_string()
         {
+            var writter = new StringWriter();
+            Console.SetOut(writter);
 
+            var input = "1 2 3 4 5 6 7 8 9";
+
+            var reverse = (string s) => Console.WriteLine(String.Concat(s.Reverse()));
+            reverse(input);
+
+            var expected = "9 8 7 6 5 4 3 2 1";
+
+            Assert.Equal(expected.ToCharArray(), writter.GetStringBuilder().ToString().Trim());
         }
+
+        [Fact]
+        public void find_product_of_two_numbers()
+        {
+            var product = (decimal d1, decimal d2) => d1 * d2;
+
+            Assert.Equal(10, product(2, 5));
+        }
+
+        [Fact]
+        public void true_string_is_equal_to_int()
+        {
+            var s = " ,. 0042";
+            var num = 0042;
+
+            var isEqual = (string s, int num) => int.Parse(s.Trim(' ',',','.')) == num;
+
+            Assert.True(isEqual(s, num));
+        }
+
+
+
+
     }
 }
